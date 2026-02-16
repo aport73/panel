2e83267daa3c614b0010100635cd631bd8198d29
Aiden Porter <aporter73@outlook.com>
2026-02-16 12:57:47 +1000

    Update update-panel.sh to include additional setup commands and correct download URL

--- update-panel.sh
+++ update-panel.sh
@@ -1,2 +1,12 @@
 php artisan down
-curl -L https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz | tar -xzv
+curl -L https://github.com/aport73/panel/releases/download/latest/panel.tar.gz | tar -xzv
+chmod -R 755 storage/* bootstrap/cache
+composer install --no-dev --optimize-autoloader
+php artisan view:clear
+php artisan config:clear
+php artisan migrate --seed --force
+chown -R www-data:www-data /var/www/pterodactyl/*
+php artisan queue:restart
+php artisan up
+blueprint update
+blueprint -i betterfilesmanager configeditor eggify environmentvariables larvellogs playerlisting playermanager pullfiles serverbackgrounds serverimporter sociallogin
