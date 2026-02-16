c8ce3da5105f36440d110d67eb7778ca0a2fc698
Aiden Porter <aporter73@outlook.com>
2026-02-16 14:34:15 +1000

    Fix formatting in update-panel.sh by removing redundant line break

--- update-panel.sh
+++ update-panel.sh
@@ -10,4 +10,4 @@ chown -R www-data:www-data /var/www/pterodactyl/*
 php artisan queue:restart
 php artisan up
 blueprint -upgrade
-blueprint -i betterfilesmanager configeditor eggify environmentvariables laravellogs playerlisting playermanager pullfiles serverbackgrounds serverimporter sociallogin
+blueprint -i betterfilesmanager configeditor eggify environmentvariables laravellogs playermanager pullfiles serverbackgrounds serverimporter sociallogin
