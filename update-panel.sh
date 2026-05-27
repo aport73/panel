63dc168244581951065206dc9dfb1c2465b60db8
Aiden Porter <aporter73@outlook.com>
2026-05-27 16:00:20 +1000

    Add build cmd to update script

--- update-panel.sh
+++ update-panel.sh
@@ -1,5 +1,7 @@
+export NODE_OPTIONS=--openssl-legacy-provider
 php artisan down
 curl -L https://github.com/aport73/panel/releases/latest/download/panel.tar.gz | tar -xzv
+yarn build:production
 chmod +x ./update-panel.sh
 chmod -R 755 storage/* bootstrap/cache
 composer install --no-dev --optimize-autoloader
